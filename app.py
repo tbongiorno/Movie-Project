1eaed1973b8b49370833b8a43925c427cd2125bf
tbongiorno <163329687+tbongiorno@users.noreply.github.com>
2026-01-13 00:40:23 -0500

    Initial Commit pt 2

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
+#Flask
+from flask import Flask, flash, redirect, render_template, request, url_for, session
+
+#SQL
+import flask_sqlalchemy
+from sqlalchemy import ForeignKey, Integer, String, Float, desc
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+#User Manager
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#Design imporst
+from flask_bootstrap import Bootstrap5
+
+#misc imports
+import requests
+from dotenv import load_dotenv
+import os
+
 #Starting File for Movie Project
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
